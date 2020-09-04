893a67e18c8b7d4c654f07f48b7973c1e28e89b7
Kevin <kevintrf@gmail.com>
2020-09-04 10:48:34 +0200

    Structured project after golang standards

---
+++ cmd/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Grumme2/D7024E/internal/test"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	test.TestPrint()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Grumme2/D7024E.git
+
+go 1.13
