6bf38d1fa15b267833af183c8a883df9fa101da6
Kevin <kevintrf@gmail.com>
2020-09-04 14:32:31 +0200

    Fixed deployScript

--- deployStack.sh
+++ deployStack.sh
@@ -1,8 +1,6 @@
 #!bin/sh
 sudo apt update
 sudo apt install docker.io -y
-sudo git clone AJIFJISFJ
-cd D7012E
-sudo docker build -t . kadlab
+sudo docker build . -t kadlab
 sudo docker swarm init
-sudo docker stack deploy --compose-file docker.compose.yml myStack
+sudo docker stack deploy --compose-file docker-compose.yml myStack
