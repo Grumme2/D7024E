dbd722c45cf859fe32053378275b1d8fc1985df2
Kevin <kevintrf@gmail.com>
2020-10-02 10:39:55 +0200

    Added JSON encoding/decoding to RPC

--- internal/d7024e/rpc.go
+++ internal/d7024e/rpc.go
@@ -1,9 +1,15 @@
 package d7024e
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type RPC struct {
 	Sender KademliaID
 	TargetAddress string
 	TargetPort string
+	MessageType string
 	Content string
 }
 
@@ -19,8 +25,24 @@ const (
 	Ok = RPCProcedureType("OK")
 )
 
-func NewRPC(sender KademliaID, targetAddress string, targetPort string, content string) RPC {
-	return RPC{Sender: sender, TargetAddress: targetAddress, TargetPort: targetPort, Content: content}
+func NewRPC(sender KademliaID, targetAddress string, targetPort string, messageType string, content string) RPC {
+	return RPC{Sender: sender, TargetAddress: targetAddress, TargetPort: targetPort, MessageType: messageType, Content: content}
 }
 
-//Add serialization support
+func JSONEncode(message RPC) []byte {
+	var jsonData []byte
+	jsonData, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return jsonData
+}
+
+func JSONDecode(message []byte) RPC {
+	var jsonData RPC
+	err := json.Unmarshal(message, &jsonData)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return jsonData
+}
