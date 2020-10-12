9abfecee0fab4a335e92bf967ffbcca9d6427d3c
Kevin Träff <Kevintrf@users.noreply.github.com>
2020-10-12 14:49:29 +0200

    Update data.go

--- internal/d7024e/data.go
+++ internal/d7024e/data.go
@@ -2,9 +2,3 @@ package d7024e
 
 // The Key value store that stores the data
 var KeyValueStore = make(map[string]string)
-
-// the static number of bytes in a KademliaID
-const IDLength = 20
-
-//The maximum size of a bucket, AKA k
-const bucketSize = 20
