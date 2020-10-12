eca626a0aad7491d236029157273a1905aad62e9
Kevin <kevintrf@gmail.com>
2020-10-12 12:45:01 +0200

    Changed SendPingMessage to SendMessage

--- internal/d7024e/network.go
+++ internal/d7024e/network.go
@@ -141,7 +141,7 @@ func (network *Network) Listen() {
 	}
 }
 
-func (network *Network) SendPingMessage(message RPC) bool {
+func (network *Network) SendMessage(message RPC) bool {
 	CONNECT := message.TargetAddress + ":8000" //Hardcoded port
 
 	s, err := net.ResolveUDPAddr("udp4", CONNECT)
