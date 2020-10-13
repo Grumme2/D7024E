b5e94617e53ed19272d90fd941af75a772199cb5
Kevin <kevintrf@gmail.com>
2020-10-13 14:09:22 +0200

    Changed main to handle network listener concurrently to cli

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func main() {
+	me := d7024e.NewContact(d7024e.NewRandomKademliaID(), "localhost")
+	rt := d7024e.NewRoutingTable(me)
+	network := d7024e.NewNetwork(rt)
+	go network.Listen()
+
 	cli := d7024e.NewCli()
 	cli.AwaitCommand()
 }
