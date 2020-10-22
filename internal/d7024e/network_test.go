ff8926e98e7c288640da8fe07155bfba0cec6731
grumme2 <agh@live.se>
2020-10-22 17:49:29 +0200

    test

--- internal/d7024e/network_test.go
+++ internal/d7024e/network_test.go
@@ -34,7 +34,7 @@ func TestKriplesJSON(t *testing.T) {
 	network := NewNetwork(rt)
 	con1 := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000001"), "localhost:8003")
 	con2 := NewContact(NewKademliaID("1111111100000000000000000000000000000001"), "localhost:8004")
-	con3 := NewContact(NewKademliaID("1111111200000000000000000000000000000001"), "localhost:8005")
+	con3 := NewContact(NewKademliaID("1111111200000000000000000000000000000001"), "localhost:8006")
 	list := []Contact{con1, con2, con3}
 	//fmt.Println(network.KTriplesJSON(list))
 	assert.Equal(t, network.KTriplesJSON(list), `[{"ID":[255,255,255,255,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],"Address":"localhost:8003","KeyValueStore":{}},{"ID":[17,17,17,17,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],"Address":"localhost:8004","KeyValueStore":{}},{"ID":[17,17,17,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],"Address":"localhost:8005","KeyValueStore":{}}]`)
