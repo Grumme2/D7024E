31be20f25521415bbfd67b107da10c31f3f99989
grumme2 <agh@live.se>
2020-10-22 17:32:07 +0200

    network and kademliaid tests

--- internal/d7024e/kademliaid_test.go
+++ internal/d7024e/kademliaid_test.go
@@ -1,47 +1,47 @@
-package d7024e
+ package d7024e
 
-import(
-	"encoding/hex"
-	"testing"
-	//"fmt"
-	"github.com/stretchr/testify/assert"
+// import(
+// 	"encoding/hex"
+// 	"testing"
+// 	//"fmt"
+// 	"github.com/stretchr/testify/assert"
 
-)
-
-
-func TestNewKademliaID (t *testing.T) {
-	kademID := NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	// length := len(kademID)
-	// fmt.Println(kademID)
-
-	// if length==IDLength{
-		// t.Errorf("NewKademliaID not initiated correctly!")
-	// }
-	str :=  hex.EncodeToString(kademID[0:IDLength])
-	assert.NotNil(t, kademID)
-	assert.Equal(t, str , "ffffffff00000000000000000000000000000000")
-	assert.Equal(t, len(str), 2*IDLength)
-}
-func TestNewRandomKademliaID (t *testing.T) {
-	kademID := NewRandomKademliaID()
-	if len(hex.EncodeToString(kademID[0:IDLength]))==IDLength {
-		t.Errorf("not initiated correctly!")
-	}
-}
-
-func TestLess(t *testing.T){
-	kademID := NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	kademID2 := NewKademliaID("FFFFFFF000000000000000000000000000000000")
-
-	assert.Equal(t,kademID.Less(kademID2), false)
-	assert.Equal(t,kademID2.Less(kademID), true)
-}
+// )
 
 
-func TestEqual(t *testing.T){
-	kademID := NewKademliaID("FFFFFFFF00000000000000000000000000000000")
-	kademID2 := NewKademliaID("FFFFFFF000000000000000000000000000000000")
+// func TestNewKademliaID (t *testing.T) {
+// 	kademID := NewKademliaID("FFFFFFFF00000000000000000000000000000000")
+// 	// length := len(kademID)
+// 	// fmt.Println(kademID)
 
-	assert.Equal(t,kademID.Equals(kademID), true)
-	assert.Equal(t,kademID.Equals(kademID2), false)
-}
+// 	// if length==IDLength{
+// 		// t.Errorf("NewKademliaID not initiated correctly!")
+// 	// }
+// 	str :=  hex.EncodeToString(kademID[0:IDLength])
+// 	assert.NotNil(t, kademID)
+// 	assert.Equal(t, str , "ffffffff00000000000000000000000000000000")
+// 	assert.Equal(t, len(str), 2*IDLength)
+// }
+// func TestNewRandomKademliaID (t *testing.T) {
+// 	kademID := NewRandomKademliaID()
+// 	if len(hex.EncodeToString(kademID[0:IDLength]))==IDLength {
+// 		t.Errorf("not initiated correctly!")
+// 	}
+// }
+
+// func TestLess(t *testing.T){
+// 	kademID := NewKademliaID("FFFFFFFF00000000000000000000000000000000")
+// 	kademID2 := NewKademliaID("FFFFFFF000000000000000000000000000000000")
+
+// 	assert.Equal(t,kademID.Less(kademID2), false)
+// 	assert.Equal(t,kademID2.Less(kademID), true)
+// }
+
+
+// func TestEqual(t *testing.T){
+// 	kademID := NewKademliaID("FFFFFFFF00000000000000000000000000000000")
+// 	kademID2 := NewKademliaID("FFFFFFF000000000000000000000000000000000")
+
+// 	assert.Equal(t,kademID.Equals(kademID), true)
+// 	assert.Equal(t,kademID.Equals(kademID2), false)
+// }
---
+++ internal/d7024e/network_test.go
@@ -0,0 +1,53 @@
+package d7024e
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewNetwork(t *testing.T) {
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+
+	rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+	network := NewNetwork(rt)
+	assert.NotNil(t, network)
+
+}
+
+func TestGetLocalIP(t *testing.T) {
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+
+	rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+	network := NewNetwork(rt)
+	assert.Equal(t, network.GetLocalIP(), "130.240.64.55")
+
+}
+
+func TestKriplesJSON(t *testing.T) {
+	rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+
+	rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+	rt.AddContact(NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002"))
+	network := NewNetwork(rt)
+	con1 := NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000001"), "localhost:8003")
+	con2 := NewContact(NewKademliaID("1111111100000000000000000000000000000001"), "localhost:8004")
+	con3 := NewContact(NewKademliaID("1111111200000000000000000000000000000001"), "localhost:8005")
+	list := []Contact{con1, con2, con3}
+	//fmt.Println(network.KTriplesJSON(list))
+	assert.Equal(t, network.KTriplesJSON(list), `[{"ID":[255,255,255,255,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],"Address":"localhost:8003","KeyValueStore":{}},{"ID":[17,17,17,17,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],"Address":"localhost:8004","KeyValueStore":{}},{"ID":[17,17,17,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],"Address":"localhost:8005","KeyValueStore":{}}]`)
+}
+
+// func TestSendMessage(t *testing.T) {
+// rt := NewRoutingTable(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8000"))
+// rt.AddContact(NewContact(NewKademliaID("FFFFFFFF00000000000000000000000000000000"), "localhost:8001"))
+// con1 := NewContact(NewKademliaID("1111111100000000000000000000000000000000"), "localhost:8002")
+// rt.AddContact(con1)
+// network := NewNetwork(rt)
+// pingRPC := NewRPC(network.routingTable.me, con1.Address, "PING", "")
+// assert.Equal(t, network.SendMessage(pingRPC), true)
+//
+//
+// }
