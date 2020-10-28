a93f56029605631b747baa17e83f99104fa28e0a
grumme2 <agh@live.se>
2020-10-28 15:44:47 +0100

    some changes

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -12,15 +12,15 @@ func main() {
 	ip := d7024e.GetLocalIP()
 	splitIP := strings.Split(ip, ".")
 	fmt.Println(splitIP)
-	if splitIP[3] == "3" {
-		mestr := "2111111300000000000000000000123000000000"
+	if splitIP[3] == "3" { // if ip ends with a 3 its the bootstrapnode
+		mestr := "2111111300000000000000000000123000000000" // hard coded bootstrap node id
 		meid = d7024e.NewKademliaID(&mestr)
 	}
 	me := d7024e.NewContact(&meid, ip)
 	rt := d7024e.NewRoutingTable(me)
 	network := d7024e.NewNetwork(rt)
 	kademlia := d7024e.NewKademlia(&network)
-	// kademlia.JoinNetwork()
+	kademlia.JoinNetwork()
 	fmt.Println(me.Address)
 	// go network.Listen()
 	// go network.CheckNodesAwaitingResponse()
